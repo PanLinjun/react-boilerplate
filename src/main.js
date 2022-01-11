13917ddf0f62abbfafc1670e619a56d47e62055a
LinjunPan <hao376151097>
2022-01-11 19:36:34 +0800

    feat

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 // import { ConfigProvider } from 'antd';
 
-const App = () => (
-  <div>
-    aaa
-  </div>
-);
+const App = () => <div></div>
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
